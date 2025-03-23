cdec61ecff5aa29c46b315a12dc3d29ac0fbdfc2
Durjoy Barua <142112376+durjoybarua5327@users.noreply.github.com>
2025-03-24 00:04:33 +0600

    commit

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from 'dotenv'
+import mongoose, { mongo } from "mongoose"
 
 dotenv.config()
 
@@ -7,9 +8,12 @@ const app = express()
 const port = process.env.PORT || 3001
 const URI = process.env.MongoDburi
 
-app.get('/', (req, res) => {
-  res.send('ha ha!')
-})
+
+try {
+    mongoose.connect(URI, {})
+} catch (error) {
+    
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
